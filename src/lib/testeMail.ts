8bed8c79a5f79e30b56e0fbbbfd001e33c261d57
maholla1 <111254539+maholla1@users.noreply.github.com>
2024-11-05 20:37:32 -0300

    Substituindo url

--- src/lib/testeMail.ts
+++ src/lib/testeMail.ts
@@ -6,6 +6,6 @@ export const send = async (email: string) => {
       to: email,
       name: "Pedro",
       subject: "Test Mail",
-      body: compileWelcomeTemplate("Pedro", "youtube.com/@sakuradev"),
+      body: compileWelcomeTemplate("Pedro", "www.youtube.com/watch?v=sENM2wA_FTg&list=RD5CcZ4ULT1Eo&index=9"),
     });
   };
